ca0179def9a98f8c561904c27e8071ae9b2780e5
AttilaBS <attila.barcellos.sipos@gmail.com>
2022-10-18 08:10:52 -0300

    jetstream install, livewire, some changes

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+
+class HomeController extends Controller
+{
+    /**
+     * The landing page of application.
+     *
+     * @return View
+     */
+    public function home(): View
+    {
+        return view('home');
+    }
+}
